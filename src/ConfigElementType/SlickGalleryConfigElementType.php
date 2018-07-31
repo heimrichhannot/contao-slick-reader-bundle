7e003be3d0043a726146a8d4ad466d02e8ef0c55
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-07-31 16:19:26 +0200

    fixed slick gallery

--- src/ConfigElementType/SlickGalleryConfigElementType.php
+++ src/ConfigElementType/SlickGalleryConfigElementType.php
@@ -9,6 +9,9 @@
 namespace HeimrichHannot\SlickReaderBundle\ConfigElementType;
 
 use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
+use Contao\FilesModel;
+use Contao\StringUtil;
+use Contao\System;
 use HeimrichHannot\ReaderBundle\ConfigElementType\ConfigElementType;
 use HeimrichHannot\ReaderBundle\Item\ItemInterface;
 use HeimrichHannot\ReaderBundle\Model\ReaderConfigElementModel;
@@ -27,7 +30,94 @@ class SlickGalleryConfigElementType implements ConfigElementType
 
     public function addToItemData(ItemInterface $item, ReaderConfigElementModel $readerConfigElement)
     {
-//        $readerConfigElement->imageSelectorField && $item->getRawValue($listConfigElement->imageSelectorField)
+        $projectDir = System::getContainer()->get('huh.utils.container')->getProjectDir() . '/';
+
+        if (!($imageSelectorField = $readerConfigElement->imageSelectorField) || !$item->getRawValue($imageSelectorField) ||
+            (!$imageField = $readerConfigElement->imageField) || !$item->getRawValue($imageField)) {
+            return;
+        }
+
+        if (null === ($config = System::getContainer()->get('huh.slick.model.config')->findByPk($readerConfigElement->slickConfig))) {
+            return;
+        }
+
+        /** @var FilesModel $filesModel */
+        $filesModel = $this->framework->getAdapter(FilesModel::class);
+
+        $images = StringUtil::deserialize($item->getRawValue($readerConfigElement->imageField), true);
+
+        if (empty($images)) {
+            return;
+        }
+
+        // prepare images
+        $preparedImages = [];
+
+        foreach ($images as $image) {
+            if (null === ($imageFile = $filesModel->findByUuid($image))) {
+                continue;
+            }
+
+            if (null === $imageFile
+                || !file_exists($projectDir . $imageFile->path)
+            ) {
+                continue;
+            }
+
+            if ($imageFile->type == 'folder') {
+                $dirname = $imageFile->path;
+
+                foreach (array_diff(scandir($projectDir . $imageFile->path), ['..', '.']) as $fileName) {
+                    if (null === ($imageFile = $filesModel->findByPath($dirname . '/' . $fileName))) {
+                        continue;
+                    }
+
+                    $preparedImages[] = $this->prepareImage($imageFile, $readerConfigElement->imgSize, $image, $imageSelectorField);
+                }
+            } else {
+                $preparedImages[] = $this->prepareImage($imageFile, $readerConfigElement->imgSize, $image, $imageSelectorField);
+            }
+        }
+
+        // create slick
+        $galleries              = $item->slickGalleries ?? [];
+        $galleries[$imageField] = [
+            'images'         => $preparedImages,
+            'dataAttributes' => System::getContainer()->get('huh.slick.config')->getAttributesFromModel($config)
+        ];
+
+        $item->setFormattedValue('slickGalleries', $galleries);
+    }
+
+    protected function prepareImage($imageFile, $imageSize, $imageField, $imageSelectorField)
+    {
+        $imageArray = [];
+
+        // Override the default image size
+        if ('' != $imageSize) {
+            $size = StringUtil::deserialize($imageSize);
+
+            if ($size[0] > 0 || $size[1] > 0 || is_numeric($size[2])) {
+                $imageArray['size'] = $imageSize;
+            }
+        }
+
+        $imageArray[$imageField] = $imageFile->path;
+
+        $templateData[$imageField] = [];
+
+        System::getContainer()->get('huh.utils.image')->addToTemplateData(
+            $imageField,
+            $imageSelectorField,
+            $templateData[$imageField],
+            $imageArray,
+            null,
+            null,
+            null,
+            $imageFile
+        );
+
+        return $templateData[$imageField];
     }
 }
 
--- src/Resources/contao/dca/tl_reader_config_element.php
+++ src/Resources/contao/dca/tl_reader_config_element.php
@@ -5,7 +5,7 @@ $dca = &$GLOBALS['TL_DCA']['tl_reader_config_element'];
 /**
  * Palettes
  */
-$dca['palettes'][\HeimrichHannot\SlickReaderBundle\Backend\ReaderConfigElement::TYPE_SLICK_GALLERY] = '{title_type_legend},title,type;{config_legend},slickConfig,imageSelectorField,imageField;';
+$dca['palettes'][\HeimrichHannot\SlickReaderBundle\Backend\ReaderConfigElement::TYPE_SLICK_GALLERY] = '{title_type_legend},title,type;{config_legend},slickConfig,imageSelectorField,imageField,imgSize;';
 
 /**
  * Fields
@@ -17,7 +17,7 @@ $fields = [
         'filter'     => true,
         'inputType'  => 'select',
         'foreignKey' => 'tl_slick_config.title',
-        'eval'       => ['tl_class' => 'w50', 'mandatory' => true, 'includeBlankOption' => true, 'submitOnChange' => true, 'chosen' => true],
+        'eval'       => ['tl_class' => 'w50', 'mandatory' => true, 'includeBlankOption' => true, 'chosen' => true],
         'sql'        => "int(10) unsigned NOT NULL default '0'"
     ],
 ];
